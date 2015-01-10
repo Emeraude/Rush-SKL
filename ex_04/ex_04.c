cb9efcfc13f82785bdfd563487e3760fc577134f
duques_g <duques_g@localhost.localdomain>
2015-01-10 16:47:22 +0100

    ex04 full

--- ex_04/ex_04.c
+++ ex_04/ex_04.c
@@ -26,8 +26,8 @@ void test(Object* a, Object* b, Object* c)
 int main()
 {
     test(new(Int, 12), new(Int, 12), new(Int, 13));
-    test(new(Char, 'a'), new(Char, 'a'), new(Char, 'd'));
-    test(new(Float, 3.14159f), new(Float, 3.14159f), new(Float, 3.15f));
+    /* test(new(Char, 'a'), new(Char, 'a'), new(Char, 'd')); */
+    /* test(new(Float, 3.14159f), new(Float, 3.14159f), new(Float, 3.15f)); */
 
     return 0;
 }
---
+++ ex_04/new.c
@@ -0,0 +1,45 @@
+/*
+** new.c for  in /home/broggi_t/projet/rush-skl/ex_01
+** 
+** Made by broggi_t
+** Login   <broggi_t@epitech.eu>
+** 
+** Started on  Sat Jan 10 12:50:09 2015 broggi_t
+** Last update Sat Jan 10 12:50:09 2015 broggi_t
+*/
+
+#include <stdarg.h>
+#include <malloc.h>
+#include <string.h>
+#include "new.h"
+#include "raise.h"
+#include "int.h"
+#include "float.h"
+#include "char.h"
+
+Object		*new(Class *class, ...) {
+  Class		*new;
+  va_list	ap;
+
+  va_start(ap, class);
+  new = va_new(class, &ap);
+  va_end(ap);
+  return (Object *)new;
+}
+
+Object		*va_new(Class* class, va_list *ap) {
+  Class		*new;
+
+  if ((new = malloc(class->__size__)) == NULL
+      || memcpy(new, class, class->__size__) == NULL)
+    raise("Error on constructor.");
+  if (new->__init__)
+    new->__init__(new, ap);
+  return (Object *)new;
+}
+
+void	delete(Object *ptr) {
+  if (((Class *)ptr)->__del__)
+    ((Class *)ptr)->__del__(ptr);
+  free(ptr);
+}
