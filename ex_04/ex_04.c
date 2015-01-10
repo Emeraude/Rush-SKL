80e67b60e2633cbfd51b28f9af1356a2034b106f
duques_g <duques_g@localhost.localdomain>
2015-01-10 17:22:34 +0100

    adding main ex04

--- ex_04/ex_04.c
+++ ex_04/ex_04.c
@@ -27,7 +27,7 @@ int main()
 {
     test(new(Int, 12), new(Int, 12), new(Int, 13));
     /* test(new(Char, 'a'), new(Char, 'a'), new(Char, 'd')); */
-    /* test(new(Float, 3.14159f), new(Float, 3.14159f), new(Float, 3.15f)); */
+    test(new(Float, 3.14159f), new(Float, 3.14159f), new(Float, 3.15f));
 
     return 0;
 }
