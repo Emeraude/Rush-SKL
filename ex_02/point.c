58d7a07e135a786b39956b5937d500399d0c9995
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-01-10 16:19:54 +0100

    fix ex2 like ex3

--- ex_02/point.c
+++ ex_02/point.c
@@ -2,7 +2,13 @@
 #include <stdarg.h>
 #include <malloc.h>
 #include <stdio.h>
+#include "raise.h"
+#include "new.h"
 #include "point.h"
+
+static void Point_ctor(Object* self, va_list *args);
+static void Point_dtor(Object* self);
+static char const *Point_to_string(Object *self);
 
 typedef struct
 {
@@ -10,28 +16,30 @@ typedef struct
     int x, y;
 } PointClass;
 
-static void Point_ctor(Object* self, va_list *args)
-{
-  ((PointClass *)self)->x = va_arg(*args, int);
-  ((PointClass *)self)->y = va_arg(*args, int);
-}
-
-static void Point_dtor(Object* self)
-{
-  (void)self;
-}
-
-static char const *Point_to_string(Object *self) {
-  char *str;
-
-  if (self)
-    asprintf(&str, "<Point (%d, %d)>", ((PointClass *)self)->x, ((PointClass *)self)->y);
-  return (char const *)str;
-}
-
 static PointClass _description = {
   { sizeof(PointClass), "Point", &Point_ctor, &Point_dtor, &Point_to_string},
     0, 0
 };
 
 Class* Point = (Class*) &_description;
+
+static void Point_ctor(Object* self, va_list *args)
+{
+  if (!self) raise("Arguments must be initialized.");
+  ((PointClass *)self)->x = va_arg(*args, int);
+  ((PointClass *)self)->y = va_arg(*args, int);
+}
+
+static void Point_dtor(Object* self)
+{
+  if (!self) raise("Arguments must be initialized.");
+}
+
+static char const *Point_to_string(Object *self)
+{
+  char *str = NULL;
+
+  if (!self) raise("Arguments must be initialized.");
+  asprintf(&str, "<Point (%d, %d)>", ((PointClass *)self)->x, ((PointClass *)self)->y);
+  return (char const *)str;
+}
--- ex_02/vertex.c
+++ ex_02/vertex.c
@@ -2,7 +2,13 @@
 #include <stdarg.h>
 #include <malloc.h>
 #include <stdio.h>
+#include "raise.h"
+#include "new.h"
 #include "vertex.h"
+
+static void Vertex_ctor(Object* self, va_list *args);
+static void Vertex_dtor(Object* self);
+static char const *Vertex_to_string_t(Object *self);
 
 typedef struct
 {
@@ -10,8 +16,16 @@ typedef struct
   int x, y, z;
 } VertexClass;
 
+static VertexClass _description = {
+  { sizeof(VertexClass), "Vertex", &Vertex_ctor, &Vertex_dtor, &Vertex_to_string_t},
+  0, 0, 0
+};
+
+Class* Vertex = (Class*) &_description;
+
 static void Vertex_ctor(Object* self, va_list *args)
 {
+  if (!self) raise("Arguments must be initialized.");
   ((VertexClass *)self)->x = va_arg(*args, int);
   ((VertexClass *)self)->y = va_arg(*args, int);
   ((VertexClass *)self)->z = va_arg(*args, int);
@@ -19,21 +33,15 @@ static void Vertex_ctor(Object* self, va_list *args)
 
 static void Vertex_dtor(Object* self)
 {
-  (void)self;
+  if (!self) raise("Arguments must be initialized.");
 }
 
-static char const *Vertex_to_string_t(Object *self) {
-  char *str;
+static char const *Vertex_to_string_t(Object *self)
+{
+  char *str = NULL;
 
-  if (self)
-    asprintf(&str, "<Vertex (%d, %d, %d)>", ((VertexClass *)self)->x,
-	     ((VertexClass *)self)->y, ((VertexClass *)self)->z);
-  return (char const *)str;
+  if (!self) raise("Arguments must be initialized.");
+  asprintf(&str, "<Vertex (%d, %d, %d)>", ((VertexClass *)self)->x,
+           ((VertexClass *)self)->y, ((VertexClass *)self)->z);
+  return (char const *)str;  
 }
-
-static VertexClass _description = {
-  { sizeof(VertexClass), "Vertex", &Vertex_ctor, &Vertex_dtor, &Vertex_to_string_t},
-  0, 0, 0
-};
-
-Class* Vertex = (Class*) &_description;
