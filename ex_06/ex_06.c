b1275962bc99ca5b9c25a3d7cace5273bf39dbb2
duques_g <duques_g@localhost.localdomain>
2015-01-11 07:39:19 +0100

    adding modified main for ex06

--- ex_06/ex_06.c
+++ ex_06/ex_06.c
@@ -2,49 +2,97 @@
 #include <stdio.h>
 #include <assert.h>
 
+#include "container.h"
 #include "list.h"
 #include "int.h"
+#include "float.h"
+#include "char.h"
 #include "new.h"
 
 
-int main()
+int		main()
 {
-    Object* list = new(List, 10, Int, 0);
-    Object* it = begin(list);
-    Object* it_end = end(list);
+  Object *listInt = new(List, 5, Int, 42);
+  Object *listFloat = new(List, 5, Float, 42.00f);
+  Object *listChar = new(List, 5, Char, 'a');
 
-    printf("list size: %zu\n", len(list));
-    setitem(list, 0, -1);
-    setitem(list, 5, 12);
-    setitem(list, 6, 13);
-    setitem(list, 9, -1);
+  printf("\n--- Test basics ---\n");
+  printf("Val 42 : %s\n", str(getitem(listInt, 4)));
+  printf("Val 42 : %s\n", str(getitem(listFloat, 4)));
+  printf("Val 42 : %s\n", str(getitem(listChar, 4)));
 
-    printf("Try to select beg/end\n");
-    printf("%s\n", str(getval(it)));
-    printf("%s\n", str(getval(it_end)));
+  printf("Len 5 : %i\n", len(listInt));
+  printf("Len 5 : %i\n", len(listFloat));
+  printf("Len 5 : %i\n", len(listChar));
 
-    printf("begin to run through\n");
-    int i = 0;
-    while (i < 10)
-      {
-        printf("the int [%u] is : %s\n", i, str(getitem(list, i)));
-        i++;
-      }
+  setitem(listInt, 4, 21);
+  setitem(listFloat, 4, 21.00f);
+  setitem(listChar, 4, 'b');
 
-    printf("\n\n--------\n");
-    i = 0;
-    while (lt(it, it_end))
-      {
-        printf("i ... %u ...\n", i);
-        printf("The int [%u] is : %s\n", i, str(getval(it)));
-        incr(it);
-        i++;
+  printf("Val 21 : %s\n", str(getitem(listInt, 4)));
+  printf("Val 21 : %s\n", str(getitem(listFloat, 4)));
+  printf("Val b : %s\n", str(getitem(listChar, 4)));
+
+  setitem(listInt, 4, 84);
+  setitem(listFloat, 4, 84.00f);
+  setitem(listChar, 4, 'z');
+
+  printf("Val 84 : %s\n", str(getitem(listInt, 4)));
+  printf("Val 84 : %s\n", str(getitem(listFloat, 4)));
+  printf("Val z : %s\n", str(getitem(listChar, 4)));
+
+  printf("\n\n");
+  Object *itInt = begin(listInt);
+  Object *itFloat = begin(listFloat);
+  Object *itChar = begin(listChar);
+
+  Object *it2Int = end(listInt);
+  Object *it2Float = end(listFloat);
+  Object *it2Char = end(listChar);
+
+  while (eq(itInt, it2Int))
+    {
+      printf("%s\n", str(getval(itInt)));
+      incr(itInt);
     }
-    printf("\n\n--------\n");
 
-    delete(it);
-    delete(it_end);
-    delete(list);
-    return 0;
+
+  Object* list = new(List, 20, Int, 0);
+  Object* it = begin(list);
+  Object* it_end = end(list);
+
+  printf("list size: %zu\n", len(list));
+  setitem(list, 0, -1);
+  setitem(list, 5, 12);
+  setitem(list, 6, 13);
+  setitem(list, 9, -1);
+
+  printf("Try to select beg/end\n");
+  printf("%s\n", str(getval(it)));
+  printf("%s\n", str(getval(it_end)));
+
+  printf("begin to run through\n");
+  int i = 0;
+  while (i < 10)
+    {
+      printf("the int [%u] is : %s\n", i, str(getitem(list, i)));
+      i++;
+    }
+
+  printf("\n\n--------\n");
+  i = 0;
+  while (lt(it, it_end))
+    {
+      printf("i ... %u ...\n", i);
+      printf("The int [%u] is : %s\n", i, str(getval(it)));
+      incr(it);
+      i++;
+  }
+  printf("\n\n--------\n");
+
+  delete(it);
+  delete(it_end);
+  delete(list);
+  return 0;
 }
 
